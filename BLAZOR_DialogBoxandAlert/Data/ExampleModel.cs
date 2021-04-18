f0e5f00268774be107aa075b6c726ae8cf0908d2
Tansu Deveciler <mss_tansu@hotmail.com>
2021-04-19 00:59:40 +0300

    Add project files.

---
+++ BLAZOR_DialogBoxandAlert/Data/ExampleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLAZOR_DialogBoxandAlert.Data
+{
+    public class ExampleModel
+    {
+        public string Product { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int UnitInStock { get; set; }
+      
+    }
+  
+}
